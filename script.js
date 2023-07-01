489a84566dabc94e966ff94250658910a93eb8fe
Great Agwaze <agwazegreat@gmail.com>
2023-07-01 07:52:11 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+// ---------------------------------------
+
+const faders = document.querySelectorAll(".fade-in");
+const appearOptions = {
+    threshold:1,
+    rootMargin: "0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach(fader =>{
+    appearOnScroll.observe(fader)
+});
+
+// ---------------------------------------------------------------------sidebar------------------------------------------------------
+
+var sidemenu = document.getElementById('sidemenu');
+
+function openmenu(){
+    sidemenu.style.right = '0';
+}
+function closemenu(){
+    sidemenu.style.right = '-200px';
+}
